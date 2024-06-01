ac289737db377f2e29fcc8163a33b52a39453caf
artcousan <artcousan@gmail.com>
2024-06-01 19:49:58 -0300

    feat: ajustes

--- app/api/app.py
+++ app/api/app.py
@@ -7,7 +7,6 @@ def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    print(f"URL  {os.getenv('SQLALCHEMY_DATABASE_URI')}")
     db.init_app(app)
     app.register_blueprint(sensor_blueprint)
     return app
