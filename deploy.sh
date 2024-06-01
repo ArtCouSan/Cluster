62eadffdb16fef125c56108a5a9da5559a36fa4d
artcousan <artcousan@gmail.com>
2024-06-01 00:48:54 -0300

    first commit

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,7 @@ if ! command -v docker &> /dev/null; then
     sudo apt-get install -y docker.io
     sudo systemctl start docker
     sudo systemctl enable docker
+    sudo usermod -aG docker $USER
     echo "Docker instalado com sucesso."
 fi
 
